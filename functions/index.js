b4da790570d06f558a36d89abb5edac01e00370f
idreamzzer <idreamzzer@gmail.com>
2019-06-13 19:14:17 +0300

    removed comments

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,6 @@ const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const config = require('./config');
 const createBot = require('./createBot');
-// const botFunction = require('./botFunction');
-// const testFunction = require('./testFunction');
 
 admin.initializeApp({
   credential: admin.credential.cert(config.googleCredentials),
@@ -12,10 +10,3 @@ admin.initializeApp({
 config.functions.forEach(botConfig => {
   exports[botConfig.name] = functions.https.onRequest(createBot(botConfig));
 });
-
-// for (let func in config.functions) {
-//   console.log(func);
-// }
-
-// exports.bot = functions.https.onRequest(botFunction);
-// exports.test = functions.https.onRequest(testFunction);
